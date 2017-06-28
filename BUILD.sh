89b5347f1505f09c1cd8dcb0a8b3bbe51979fe88
Jim Huang <jserv.tw@gmail.com>
2017-06-28 16:38:02 +0800

    Enable gold linker with Identical Code Folding (ICF)

--- BUILD.sh
+++ BUILD.sh
@@ -5,7 +5,8 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 export CROSS_COMPILE=aarch64-linux-gnu-
 
-ARCH_FLAGS="-march=armv8-a+crc+crypto"
+# FIXME: not specific to Cavium ThunderX
+ARCH_FLAGS="-march=armv8-a+crc+crypto -mtune=thunderx"
 
 set -x
 make \
@@ -13,9 +14,9 @@ make \
   CXX=${CROSS_COMPILE}g++ \
   AR=${CROSS_COMPILE}ar \
   STRIP=${CROSS_COMPILE}strip \
-  EXTRA_CFLAGS="${ARCH_FLAGS}" \
+  EXTRA_CFLAGS="${ARCH_FLAGS} -fuse-ld=gold" \
   EXTRA_CXXFLAGS="${ARCH_FLAGS} -static-libstdc++ -fuse-ld=gold" \
-  EXTRA_LDFLAGS="-Wl,-Bsymbolic-functions" \
+  EXTRA_LDFLAGS="-Wl,-Bsymbolic-functions -Wl,--icf=all" \
   EXTRA_AMFLAGS="--host=aarch64-linux-gnu" \
   DEBUG_LEVEL=0 \
   rocksdbjavastatic -j8
